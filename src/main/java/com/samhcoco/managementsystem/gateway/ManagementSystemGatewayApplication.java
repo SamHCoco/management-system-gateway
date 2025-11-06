b12774ef2e8b75dcacf8a6bad447a206e1aedddb
Samuel Coco <Samuel.HenriAC@outlook.com>
2025-11-06 09:08:10 +0000

    Routing for 'employee-service' added

---
+++ src/main/java/com/samhcoco/managementsystem/gateway/ManagementSystemGatewayApplication.java
@@ -0,0 +1,15 @@
+package com.samhcoco.managementsystem.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class ManagementSystemGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManagementSystemGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/samhcoco/managementsystem/gateway/ManagementSystemGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.samhcoco.managementsystem.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagementSystemGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
